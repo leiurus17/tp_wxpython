ac2669f49dab2f8b472253ac06e44d29216baf98
Marlon <martianworm17@gmail.com>
2017-11-13 20:09:54 +0800

    Add panel

--- wxpython/demo_wxframe2/gui_src.py
+++ wxpython/demo_wxframe2/gui_src.py
@@ -53,3 +53,27 @@ class MyFrame ( wx.Frame ):
         
     
 
+###########################################################################
+## Class MyPanel3
+###########################################################################
+
+class MyPanel3 ( wx.Panel ):
+    
+    def __init__( self, parent ):
+        wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL )
+        
+        self.m_mgr = wx.aui.AuiManager()
+        self.m_mgr.SetManagedWindow( self )
+        self.m_mgr.SetFlags(wx.aui.AUI_MGR_DEFAULT)
+        
+        self.m_button1 = wx.Button( self, wx.ID_ANY, u"MyButton", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_mgr.AddPane( self.m_button1, wx.aui.AuiPaneInfo() .Left() .PinButton( True ).Float().FloatingPosition( wx.Point( 516,403 ) ).Resizable().FloatingSize( wx.Size( 104,65 ) ) )
+        
+        
+        self.m_mgr.Update()
+    
+    def __del__( self ):
+        self.m_mgr.UnInit()
+        
+    
+
